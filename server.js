768c17a34ce807ef427b570f8d2e18b0fa843bb8
smilear2015 <smilear2015@gmail.com>
2017-02-07 15:09:44 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
-var Pool = require('pg').Pool;
 
-var app = express();
-app.use(morgan('combined'));
 
 
 
